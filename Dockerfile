794afdd35462c0c207826e272736e6493d8b3c01
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-02-02 13:34:06 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Based on python:alpine as of February 2021
+FROM github/super-linter:v3.14.5
+
+LABEL org.opencontainers.image.authors="Jon Zeolla"
+LABEL org.opencontainers.image.licenses="MIT"
+LABEL org.opencontainers.image.vendor="Seiso"
+LABEL org.opencontainers.image.version="${VERSION}"
+LABEL org.opencontainers.image.title="goat"
+LABEL org.opencontainers.image.description="The Grand Opinionated AutoTester (GOAT) automatically applies Seiso's standard testing"
+LABEL org.opencontainers.image.url="https://seisollc.com"
+LABEL org.opencontainers.image.source="https://github.com/SeisoLLC/goat"
+LABEL org.opencontainers.image.revision="${COMMIT_HASH}"
+
+WORKDIR /tmp/lint/
+ENV PIP_NO_CACHE_DIR=1
+
+COPY etc/ /usr/local/etc/
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+COPY Pipfile.lock Pipfile.local
+
+RUN pipenv install --system --deploy --ignore-pipfile
+
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
---
+++ tasks.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+Task execution tool & library
+"""
+
+import json
+import sys
+from logging import basicConfig, getLogger
+from pathlib import Path
+
+import docker
+import git
+from invoke import task
+
+
+# Helper functions
+def opinionated_docker_run(
+    *,
+    image: str,
+    command: str = "",
+    volumes: dict = {},
+    working_dir: str = "/tmp/lint/",
+    auto_remove: bool = False,
+    detach: bool = True,
+    environment: dict = {},
+    expected_exit: int = 0,
+):
+    """Perform an opinionated docker run"""
+    container = CLIENT.containers.run(
+        auto_remove=auto_remove,
+        command=command,
+        detach=detach,
+        environment=environment,
+        image=image,
+        volumes=volumes,
+        working_dir=working_dir,
+    )
+
+    if not auto_remove:
+        response = container.wait(condition="not-running")
+        decoded_response = container.logs().decode("utf-8")
+        response["logs"] = decoded_response.strip().replace("\n", "  ")
+        container.remove()
+        if not is_status_expected(expected=expected_exit, response=response):
+            sys.exit(response["StatusCode"])
+
+    LOG.info("The %s scan completed successfully", image)
+
+
+def is_status_expected(*, expected: int, response: dict) -> bool:
+    """Check to see if the status code was expected"""
+    actual = response["StatusCode"]
+
+    if expected != actual:
+        LOG.error(
+            "Received a baaaaaaaaah-d status code from docker (%s); additional details: %s",
+            response["StatusCode"],
+            response["logs"],
+        )
+        return False
+
+    return True
+
+
+# Globals
+CWD = Path(".").absolute()
+VERSION = "0.1.0"
+NAME = "goat"
+
+LOG_FORMAT = json.dumps(
+    {
+        "timestamp": "%(asctime)s",
+        "namespace": "%(name)s",
+        "loglevel": "%(levelname)s",
+        "message": "%(message)s",
+    }
+)
+basicConfig(level="INFO", format=LOG_FORMAT)
+LOG = getLogger("seiso." + NAME)
+
+# git
+REPO = git.Repo(CWD)
+COMMIT_HASH = REPO.head.object.hexsha
+
+# Docker
+CLIENT = docker.from_env()
+IMAGE = "seiso/" + NAME
+
+
+# Tasks
+@task
+def goat(c, local=False):  # pylint: disable=unused-argument
+    """Build and run the goat"""
+    if local:
+        environment = {"RUN_LOCAL": "true"}
+
+    buildargs = {"VERSION": VERSION, "COMMIT_HASH": COMMIT_HASH}
+
+    LOG.info("Building %s...", IMAGE)
+    CLIENT.images.build(path=str(CWD), rm=True, tag=IMAGE, buildargs=buildargs)
+    working_dir = "/tmp/lint/"
+    volumes = {CWD: {"bind": working_dir, "mode": "ro"}}
+    opinionated_docker_run(
+        image=IMAGE, volumes=volumes, working_dir=working_dir, environment=environment,
+    )
+
+
+@task
+def publish(c):  # pylint: disable=unused-argument
+    """Publish the goat"""
+    repository = IMAGE
+    LOG.info("Pushing %s to docker hub...", repository)
+    CLIENT.images.push(repository=repository)
+    LOG.info("Done publishing the %s Docker image", IMAGE)
