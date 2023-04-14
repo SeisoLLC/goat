2af6f58c57fd70a72d478e71b842974bb9c5fabf
ChiefHolland <keith.holland@SeisoLLC.com>
2023-04-14 12:27:16 -0500

    testing4

--- entrypoint.sh
+++ entrypoint.sh
@@ -133,7 +133,7 @@ function lint_loop() {
   input="/etc/opt/goat/linters.txt"
 
   declare -A linters 
-  cat /etc/opt/goat/test.py
+
   while IFS="=" read -d $'\n' -r k v
   do 
     linters[$k]="$v"
@@ -142,11 +142,9 @@ function lint_loop() {
   for i in "${!linters[@]}"
   do
     while read -r file; do
-      bash -c "$i ${linters[$i]}"
+      $i ${linters[$i]}
     done < <(find . -path "./.git" -prune -or -type f)
   done
-
-  cat /etc/opt/goat/test.py
 }
 
 setup_environment
