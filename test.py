09b83662b92ff00a54bebf0fecedb734acfb4710
ChiefHolland <keith.holland@SeisoLLC.com>
2023-04-18 14:53:58 -0500

    testing4

---
+++ test.py
@@ -0,0 +1,10 @@
+# Import sys module
+import sys
+
+# Total number of arguments
+print("Total arguments:", len(sys.argv))
+
+print("Argument values are:")
+# Iterate command-line arguments using for loop
+for i in sys.argv:
+    print(i)
