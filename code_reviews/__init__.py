ef4ae5d59191755b878766cf1e06ec25acd3a3e5
ChiefHolland <keith.holland@SeisoLLC.com>
2024-05-01 10:23:37 -0500

    updating code review py

--- code_reviews/__init__.py
+++ code_reviews/__init__.py
@@ -1,14 +1,6 @@
 """
 salacious-code-reviews init
 """
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 66f2589 (adding code reviews)
-=======
-
->>>>>>> 020471e (Adding dependencies)
 __maintainer__ = "Seiso"
 __project_name__ = "goat_code_reviews"
 __version__ = "2023.11.01"
